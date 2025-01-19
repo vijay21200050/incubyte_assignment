c6b7766b9b836ea4d441cead14bf20b952aae65f
vijay21200050 <vijay21200050@gmail.com>
2025-01-19 20:30:58 +0530

    handling custom delimiter

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -3,6 +3,10 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.strip.empty?
 
+    if numbers.start_with?('//')
+      numbers = numbers.slice(2..-1).gsub(';', ',')
+    end
+
     numbers.gsub!("\n", ',')
     num_array = numbers.split(',').map(&:to_i)
     num_array.sum
