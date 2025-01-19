63fc79b4ef8bb50358154a8b00e90fc96e584ccf
vijay21200050 <vijay21200050@gmail.com>
2025-01-19 20:11:57 +0530

    handling multiple numbers in the add params

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -2,12 +2,9 @@ class StringCalculator
 
   def add(numbers)
     return 0 if numbers.strip.empty?
-    if numbers.split(',').size == 1
-      numbers.to_i
-    else
-      num_array = numbers.split(',')
-      num_array[0].to_i + num_array[1].to_i
-    end
+
+    num_array = numbers.split('').map(&:to_i)
+    num_array.sum
   end
 
 end
