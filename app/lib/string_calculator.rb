949b3628e11d260aa4821fcce5ca82cff57c30fa
vijay21200050 <vijay21200050@gmail.com>
2025-01-19 20:50:17 +0530

    handling delimiters of any length

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -3,12 +3,22 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.strip.empty?
 
+    delimiter = ","
+
     if numbers.start_with?('//')
-      numbers = numbers.slice(2..-1).gsub(';', ',')
+      match = numbers.match(%r{^//\[(.*?)\]\n(.*)})
+      if match
+        delimiter = match[1]
+        numbers = match[2]
+      else
+        match = numbers.match(%r{^//(.*?)\n(.*)})
+        delimiter = match[1] if match
+        numbers = match[2] if match
+      end
     end
 
-    numbers.gsub!("\n", ',')
-    num_array = numbers.split(',').map(&:to_i)
+    numbers.gsub!("\n", delimiter)
+    num_array = numbers.split(delimiter).map(&:to_i)
     negatives = num_array.select(&:negative?)
     raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
     num_array.reject! { |n| n > 1000 }
